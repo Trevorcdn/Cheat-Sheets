ed3fd467ce46cc4a8041fbdb3024a6dfe221d99d
EC2 Default User <ec2-user@ip-172-31-16-141.us-west-2.compute.internal>
2020-01-23 03:16:02 +0000

    Added file and case syntax

--- bashCheatSheet.sh
+++ bashCheatSheet.sh
@@ -12,14 +12,11 @@ NAME = "Trevor"
 read -p "Enter your pet's name: " PETNAME
 
 #Conditionals - SIMPLE IF STATEMENT
-## if [ ]
-## then
-## fi
-## fi is used to signal end of if statement
 if [ "$PETNAME" == "Pop"]
 then
     echo " Your pet's name is Pop"
 fi
+## fi is used to signal end of if statement
 
 ## IF-ELSE
 if [ "$PETNAME" == "Pop"]
@@ -29,13 +26,71 @@ else
     echo " Your pet's name is not Pop"
 fi
 
-## IF-ELIF-Else
+## IF-ELIF-ELSE
 if [ "$PETNAME" == "Pop"]
 then
     echo " Your pet's name is Pop"
 elif [ "$PETNAME" == "Top"]
 then
-    
+    echo " Your pet's name is Top"
 else
-    echo " Your pet's name is not Pop"
+    echo " Your pet's name is not Pop nor Top"
 fi
+
+########
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values ARE NOT equal
+# val1 -gt val2 Returns true if val1 is greater than val2
+# val1 -ge val2 Returns true if val1 is greater than or equal to val2
+# val1 -lt val2 Returns true if val1 is less than val2
+# val1 -le val2 Returns true if val1 is lessthan or equal to val2
+########
+
+#COMPARISON
+NUM1 = 31
+NUM2 = 5
+if ["$NUM1" - gt "$NUM2"]
+then
+    echo "$NUM1 is greater than $NUM2"
+else
+    echo "$NUM2 is greater than $NUM1"
+fi
+
+
+## FILE CONDITIONS
+
+########
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+########
+
+FILE = "test.txt"
+if [ -f "$FILE"] #checking if FILE is a file with -f
+then
+    echo "$FILE is a file"
+else
+    echo "$FILE is NOT a file"
+fi
+
+
+## CASE STATEMENT
+read -p "Are you a dog person or a cat person?" ANSWER
+case "$Answer" in
+    [dD] | [dD][oO][gG]) #kinda like a regex expression for dog,Dog,DOg, DoG etc
+        echo "You are a dog person"
+        ;;
+    [cC] | [cC][aA][tT])
+        echo "You are a cat person"
+        ;;
+    *)
+    echo "Please enter dog or cat"
+    ;;
+esac
+
